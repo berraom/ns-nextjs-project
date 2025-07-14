97518c5cbf7f51b5496eb857ca4feec24058e4b1
berraom <berrameroglu@icloud.com>
2025-07-14 09:23:42 +0300

    style: refine phone input styles and layout adjustments

--- src/components/input/_style.tsx
+++ src/components/input/_style.tsx
@@ -4,71 +4,69 @@ import PhoneInput from "react-phone-number-input";
 export const PhoneWrapper = styled.div`
   display: flex;
   gap: 8px;
-  align-items: center;
   margin-top: 50px;
+  align-items: center;
+  justify-content: center;
   width: 492px;
   height: 40px;
 `;
 
 export const CountryInputWrapper = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  border: 1px solid #d0d3d4;
+  flow:row;
+  display:flex;
+  width:162px;
+  height:40px;
+  border: 1px solid #E8EBEC;
+  gap: 10px;
   border-radius: 8px;
+  padding: 8px 16px 8px 16px;
+  align-items: center;
 `;
 
 export const StyledPhoneInput = styled(PhoneInput)`
-  height: 40px;
-  width: 162px;
-  border: none;
-  padding: 8px 16px;
-  font-size: 16px;
-  background-color: #ffffff;
-
+  
+  height: 24px;
+  width: 130px;
+  gap: 10px;
+  
   .PhoneInputInput {
-    border: none;
-    outline: none;
-    font-size: 16px;
-    width: 100%;
-  }
-`;
-
-export const Input = styled.input`
-  flex: 1;
-  height: 40px;
-  padding: 8px 16px;
-  font-size: 16px;
-  border: 1px solid #d0d3d4;
-  border-radius: 8px;
+  border: none;
   outline: none;
+  width: 66px;
+  height:24px;
+  font-weight:400;
+  font-size:16px;
+  line-height: 24px;
+  color: #465D69;
+}
 
-  &::placeholder {
-    color: #a0a3a5;
-  }
-
-  &:focus {
-    border-color: #007bff;
-    box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.25);
   }
 `;
+
+
 
 export const InputBase = styled.div`
   display: flex;
-  flex-direction: column; /* İstersen 'row' yapabilirsin */
+  flex-direction: row; 
+  gap: 8px;
+  align-items: center;
+  width: 100%; 
 `;
 
 export const InputField = styled.input`
   flex: 1;
+  width: 322px;
   height: 40px;
-  padding: 8px 16px;
-  font-size: 16px;
-  border: 1px solid #d0d3d4;
+  padding: 8px 16px 8px 16px;
+  border: 1px solid #E8EBEC;
   border-radius: 8px;
   outline: none;
 
   &::placeholder {
-    color: #a0a3a5;
+    color: #465D69;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
   }
 
   &:focus {
